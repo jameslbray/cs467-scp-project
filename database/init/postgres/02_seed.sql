607f501ff44105323799e6edd922ea5a0606b494
Nicholas Laustrup <124007393+nicklaustrup@users.noreply.github.com>
2025-04-19 11:00:30 -0700

    Added user_status seed data to database/init/postgres/02_seed
    modified:   database/init/postgres/02_seed.sql
    
    Signed-off-by: Nicholas Laustrup <124007393+nicklaustrup@users.noreply.github.com>

---
+++ database/init/postgres/02_seed.sql
@@ -0,0 +1,7 @@
+-- Seed data for user_status table
+INSERT INTO user_status (user_id, status, last_status_change) VALUES
+(1, 'online', '2025-04-19 14:22:10'),
+(2, 'away', '2025-04-19 12:15:30'),
+(3, 'offline', '2025-04-19 08:40:22'),
+(4, 'online', '2025-04-19 10:30:15'),
+(5, 'away', '2025-04-19 16:50:45');
